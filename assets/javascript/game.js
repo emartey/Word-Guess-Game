f7ef9ab751c24e164cf86d0bb7d5321ed9eda47e
Edward Martey <martey8709@gmail.com>
2019-09-28 17:36:14 -0500

    Javascript start. Variables assigned

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,55 @@
 // GLOBAL FUNCTIONS
 // ===========================================================OBAL VARIABLES
+// Arrays and variables for handling data
+var wordPossib = ["bermuda", "ghana", "california", "greece", "guinea"];
+var selectWord = "";
+var lettersinWord = [];
+var numBlank = 0;
+var correctOne = [];
+var wrongOne = [];
+
+// Game counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 15;
+
 // FUNCTIONS
 // ===========================================================
-// MAIN PROCESSS
+function startGame() {
+    selectWord = wordPossib[Math.floor(Math.random() * wordPossib.length)];
+    lettersinWord = selectWord.split("");
+    numBlank = lettersinWord.length;
+
+
+    //reset
+    guessesLeft = 15;
+    wrongOne = [];
+    correctOne = [];
+
+    //Pop blanks and successes with right number of blank.
+    for (var i = 0; i < numBlank; i++) {
+        correctOne.push("_");
+    }
+
+    // Change HTML
+    document.getElementById("word").innerHTML = correctOne.join(" ");
+    document.getElementById("quantityGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCount").innerHTML = winCount;
+    document.getElementById("lossCount").innerHTML = lossCount;
+
+
+    //tests
+    console.log(wordPossib);
+    console.log(selectWord);
+    console.log(lettersinWord);
+    console.log(numBlank);
+    console.log(correctOne);
+
+}
+
+
+
+
+
+// MAIN PROCESS
+startGame();
